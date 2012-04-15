09d9aa4079d4cbae269fdfb3704b1f3d43d189b9
agargenta <sasa@marakana.com>
2012-04-14 22:18:00 -0700

    Added inline comments

--- common.mk
+++ common.mk
@@ -1,15 +1,22 @@
+# Since this file can also be referenced by alpha-sdk_addon 
+# we cannot assume LOCAL_PATH points to the directory where
+# this file is located. Instead, we create another variable
+# to capture this directory.
 MY_PATH := $(LOCAL_PATH)/../alpha
 
+# Include all makefiles in sub-directories (one level deep)
 include $(call all-subdir-makefiles)
 
 # Enable our custom kernel
 LOCAL_KERNEL := $(MY_PATH)/kernel
 PRODUCT_COPY_FILES += $(LOCAL_KERNEL):kernel
 
-# Copy our init and ueventd configuration files to the root file system (ramdisk)
+# Copy our init and ueventd configuration files to the root
+# file system (ramdisk.img -> boot.img)
 PRODUCT_COPY_FILES += $(MY_PATH)/init.marakanaalphaboard.rc:root/init.marakanaalphaboard.rc
 PRODUCT_COPY_FILES += $(MY_PATH)/ueventd.marakanaalphaboard.rc:root/ueventd.marakanaalphaboard.rc
 
+# Add these packages to this product
 PRODUCT_PACKAGES += libmrknlog
 PRODUCT_PACKAGES += mrknlog
 PRODUCT_PACKAGES += mrknlogd
--- full_alpha.mk
+++ full_alpha.mk
@@ -1,4 +1,8 @@
+# Defines a list of languages to be supported by our device
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_small.mk)
+
+# Defines the rules for building the base Android platform,
+# but itself is not specialized for any particular device
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
 
 # Discard inherited values and use our own instead.
@@ -6,9 +10,9 @@ PRODUCT_NAME := full_marakana_alpha
 PRODUCT_DEVICE := alpha
 PRODUCT_MODEL := Full Marakana Alpha Image for Emulator
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
+# Include the common definitions and packages
 include $(LOCAL_PATH)/common.mk
 
+# Add our device-specific packages
 PRODUCT_PACKAGES += MrknLogLibClient 
 PRODUCT_PACKAGES += MrknLogServiceClient 
