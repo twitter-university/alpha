7891dbd2405f0f849071659a9332f54c93f36af1
Aleksandar Gargenta <sasa@gargenta.com>
2012-10-05 09:08:05 -0700

    Added support for dumpsys

--- app/MrknLogService/src/com/marakana/android/logservice/ILogServiceImpl.java
+++ app/MrknLogService/src/com/marakana/android/logservice/ILogServiceImpl.java
@@ -2,11 +2,15 @@ package com.marakana.android.logservice;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Binder;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Slog;
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import com.marakana.android.service.log.ILogListener;
 import com.marakana.android.service.log.ILogService;
 import com.marakana.android.lib.log.LibLog;
@@ -17,6 +21,7 @@ class ILogServiceImpl extends ILogService.Stub {
   private static final int INCREMENTAL_TIMEOUT = 2 * 1000;
   private static final boolean DEBUG = false;
   private final Map<IBinder, ListenerTracker> listeners = new HashMap<IBinder, ListenerTracker>();
+  private final AtomicInteger flushCounter = new AtomicInteger();
   private final Context context;
   private LogServiceThread logServiceThread;
   private int nativeHandle;
@@ -35,6 +40,7 @@ class ILogServiceImpl extends ILogService.Stub {
     this.context.enforceCallingOrSelfPermission(Manifest.permission.FLUSH_LOG, "Flush somewhere else");
     if (DEBUG) Slog.d(TAG, "Flushing log.");
     LibLog.flushLog(this.nativeHandle);
+    this.flushCounter.incrementAndGet();
   }
 
   public int getUsedLogSize() {
@@ -85,6 +91,39 @@ class ILogServiceImpl extends ILogService.Stub {
           }
         }
       }
+    }
+  }
+  
+  protected void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
+    if (this.context.checkCallingOrSelfPermission(android.Manifest.permission.DUMP) != 
+        PackageManager.PERMISSION_GRANTED) {
+      pw.print("Permission Denial: can't dump ILogService from from pid=");
+      pw.print(Binder.getCallingPid());
+      pw.print(", uid=");
+      pw.println(Binder.getCallingUid());
+      return;
+    } else if (args.length > 0 && args[0] != null) {
+      if (args[0].equals("flush-count")) {
+        pw.println(this.flushCounter.get());
+      } else if (args[0].equals("used-size")) {
+        pw.println(this.getUsedLogSize());
+      } else if (args[0].equals("total-size")) {
+        pw.println(this.getTotalLogSize());
+      } else if (args[0].equals("listeners")) {
+        pw.println(this.listeners.size());
+      } else {
+        pw.println("Usage: ILogService [flush-count|used-size|total-size|listeners]");
+      }
+    } else {
+      pw.println("ILogServiceState:");
+      pw.print("Flush count: ");
+      pw.println(this.flushCounter.get());
+      pw.print("Used log size: ");
+      pw.println(this.getUsedLogSize());
+      pw.print("Total log size: ");
+      pw.println(this.getTotalLogSize());
+      pw.print("Listeners: ");
+      pw.println(this.listeners.size());
     }
   }
 
