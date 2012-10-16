5446d0396a3aea84190555696e8d6c634409ed1c
Aleksandar Gargenta <sasa@gargenta.com>
2012-10-16 00:04:59 -0700

    Split packages into a separate file for easier inclusion into existing devices

--- common.mk
+++ common.mk
@@ -16,19 +16,5 @@ PRODUCT_COPY_FILES += $(LOCAL_KERNEL):kernel
 PRODUCT_COPY_FILES += $(MY_PATH)/init.marakanaalphaboard.rc:root/init.marakanaalphaboard.rc
 PRODUCT_COPY_FILES += $(MY_PATH)/ueventd.marakanaalphaboard.rc:root/ueventd.marakanaalphaboard.rc
 
-# Add these packages to this product
-PRODUCT_PACKAGES += libmrknlog
-PRODUCT_PACKAGES += mrknlog
-PRODUCT_PACKAGES += mrknlogd
-
-PRODUCT_PACKAGES += \
-	com.marakana.android.lib.log \
-	com.marakana.android.lib.log.xml \
-	libmrknlog_jni
-
-PRODUCT_PACKAGES += \
-	com.marakana.android.service.log \
-	com.marakana.android.service.log.xml
-
-PRODUCT_PACKAGES += MrknLogService
+include $(LOCAL_PATH)/packages.mk
 
---
+++ packages.mk
@@ -0,0 +1,19 @@
+# Add these packages to this product
+PRODUCT_PACKAGES += mrknlog.default
+PRODUCT_PACKAGES += mrknlog
+PRODUCT_PACKAGES += mrknlogd
+
+PRODUCT_PACKAGES += \
+    com.marakana.android.lib.log \
+    com.marakana.android.lib.log.xml \
+    libmrknlog_jni
+
+PRODUCT_PACKAGES += \
+    com.marakana.android.service.log \
+    com.marakana.android.service.log.xml
+
+PRODUCT_PACKAGES += MrknLogService
+PRODUCT_PACKAGES += MrknLogServiceClient
+
+PRODUCT_PACKAGES += MrknLogLibClient
+
