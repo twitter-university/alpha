51f9e161145ace90f5aea8adcf35bc01ab213e17
Aleksandar Gargenta <sasa@gargenta.com>
2012-10-18 13:17:42 -0700

    Fixed path

--- common.mk
+++ common.mk
@@ -16,5 +16,4 @@ PRODUCT_COPY_FILES += $(LOCAL_KERNEL):kernel
 PRODUCT_COPY_FILES += $(MY_PATH)/init.marakanaalphaboard.rc:root/init.marakanaalphaboard.rc
 PRODUCT_COPY_FILES += $(MY_PATH)/ueventd.marakanaalphaboard.rc:root/ueventd.marakanaalphaboard.rc
 
-include $(LOCAL_PATH)/packages.mk
-
+include $(MY_PATH)/packages.mk
