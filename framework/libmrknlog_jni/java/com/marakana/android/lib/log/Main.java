510a1464a3e91bf3b7f7e36cf6b3bfc9f4e14384
Aleksandar Gargenta <sasa@gargenta.com>
2012-10-19 13:30:17 -0700

    oops. fixed a bug

--- framework/libmrknlog_jni/java/com/marakana/android/lib/log/Main.java
+++ framework/libmrknlog_jni/java/com/marakana/android/lib/log/Main.java
@@ -9,11 +9,11 @@ public class Main {
           int usedSize = LibLog.getUsedLogSize(handle);
           int totalSize = LibLog.getTotalLogSize(handle);
           LibLog.flushLog(handle);
+          System.out.printf("Flushed log. Previously it was consuming %d of %d bytes\n",
+              usedSize, totalSize);
       } finally {
           LibLog.close(handle);
       }
-      System.out.printf("Flushed log. Previously it was consuming %d of %d bytes\n",
-          usedSize, totalSize);
     } catch (LibLogException e) {
 	  System.err.println("Failed to flush the log");
 	  e.printStackTrace();
